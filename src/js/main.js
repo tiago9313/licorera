044315ad4a4debc93fa660a79efd2fefb4d1fae2
unknown <sar9313@gmail.com>
2023-10-20 23:13:10 -0500

    Archivos

---
+++ src/js/main.js
@@ -0,0 +1,32 @@
+new Swiper('.provee-slider', {
+    speed: 400,
+    loop: true,
+    autoplay: {
+        delay: 5000,
+        disableOnInteraction: false
+    },
+    slidesPerView: 'auto',
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+        clickable: true
+    },
+    breakpoints: {
+        320: {
+            slidesPerView: 2,
+            spaceBetween: 40
+        },
+        480: {
+            slidesPerView: 3,
+            spaceBetween: 60
+        },
+        640: {
+            slidesPerView: 4,
+            spaceBetween: 80
+        },
+        992: {
+            slidesPerView: 6,
+            spaceBetween: 120
+        }
+    }
+});
